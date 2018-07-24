5bb646185a75a0a36a1f137ce8b781accf818b8c
Waldomiro Seabra <wjgs@icomp.ufam.edu.br>
2018-07-24 12:03:52 -0400

    edição das rotas calcs/fat e adicionando a rota calcs/fib

--- routes/calcs.js
+++ routes/calcs.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
 function fat (n) { // TODO
-  return n;
+	var m = n
+	while (n > 1) {
+		m = m * (n-1);
+		n--;
+	}
+  return m;
+}
+
+function fib (n) {
+	var a = 0, b = 1, aux;
+	for (var i = 0; i < n; i++) {
+		aux = a + b;
+		a = b;
+		b = aux;
+	}
+	return aux;
 }
 
 router.post('/fat', (req, res) => {
@@ -12,6 +27,16 @@ router.post('/fat', (req, res) => {
   }
 
   res.json({result: fat(n)});
+});
+
+router.post('/fib', (req, res) => {
+  const {n} = req.body;
+
+  if (!n) {
+    res.sendStatus(400);
+  }
+
+  res.json({result: fib(n)});
 });
 
 module.exports = router;
