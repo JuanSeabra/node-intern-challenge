315c9cb3120fa3e67562150336996b623d9a0dc2
Waldomiro Seabra <wjgs@icomp.ufam.edu.br>
2018-07-24 15:38:43 -0400

    alterando log

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const calcsRouter = require('./routes/calcs');
+const livroRouter = require('./routes/livro');
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -12,12 +13,21 @@ app.use(bodyParser.urlencoded({extended: false}));
 // ----- Middlewares -----
 
 
+app.use(function (req, res, next) {
+  var d = new Date(1970, 1, 1, 0, 0, 0, 0);
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log('LOG: ', Date(Date.now())+ ' ' + fullUrl)
+  next()
+})
+
 // ----- Routes -----
 app.get('/hello', (req, res) => {
   res.status(200).send({message: 'hello world'});
 });
 
 app.use('/calcs', calcsRouter);
+
+app.use('/livro', livroRouter)
 // ----- Routes -----
 
 
